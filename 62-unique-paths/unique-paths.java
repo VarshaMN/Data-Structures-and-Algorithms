4101610cd5aa1e4ffd6d487b5931a516b12c407b
VarshaMN <69746392+VarshaMN@users.noreply.github.com>
2025-04-18 23:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (60.92%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,12 +1,7 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return topDown(m-1,n-1,dp);
+       
+        return bottomUp(m,n);
     }
 
     public int rec(int i,int j){
@@ -36,5 +31,22 @@ class Solution {
         
         return dp[i][j]=up+left;
 
+    }
+    int bottomUp(int m, int n){
+        int[][] dp=new int[m][n];
+       
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0) continue;
+                 int up=0,left=0;
+
+                 if(i>0) up=dp[i-1][j];
+                 if(j>0) left=dp[i][j-1];
+                 dp[i][j]=up+left;
+
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
