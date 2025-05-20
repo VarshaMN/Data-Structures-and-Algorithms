1b3a439951a43ce65d18930f027fc91063f6bd2f
VarshaMN <69746392+VarshaMN@users.noreply.github.com>
2025-05-20 22:20:20 +0530

    Time: 21 ms (54.15%) | Memory: 51 MB (42.38%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,14 +1,24 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int s1=text1.length();
-        int s2=text2.length();
-        int[][] dp=new int[s1][s2];
-        for(int i=0;i<s1;i++){
-            for(int j=0;j<s2;j++){
-                dp[i][j]=-1;
+        int n=text1.length();
+        int m=text2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+              if(text1.charAt(i-1)==text2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+        
+        //not matching
+       
+             else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return topDown(text1,text2,s1-1,s2-1,dp);
+        return dp[n][m];
     }
     int rec(String s1,String s2,int i,int j){
         if(i<0 || j<0){
@@ -38,7 +48,8 @@ class Solution {
        
             return dp[i][j]=Math.max(topDown(s1,s2,i-1,j,dp),topDown(s1,s2,i,j-1,dp));
         
-
+        //tc:o(nxm)+o(n+m)stack space sc:o(nxm)
 
     }
+  
 }
