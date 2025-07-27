f497ca5934ca6cf54f9c6c6052e96d8fad922891
VarshaMN <69746392+VarshaMN@users.noreply.github.com>
2025-07-27 14:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (53.86%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,29 +15,37 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node curr=head;
-        Node prev=null;
-        Node res=null;
-        Map<Node,Node> map=new HashMap<>();
+        if (head == null) return null;
 
-        while(curr!=null){
-            Node newNode=new Node(curr.val);
-            map.put(curr,newNode);
-            if(prev==null) res=newNode;
-            else prev.next=newNode;
-            prev=newNode;
-            curr=curr.next;
-
-
+        // Step 1: Interweave cloned nodes with the original list
+        Node curr = head;
+        while (curr != null) {
+            Node newNode = new Node(curr.val);
+            newNode.next = curr.next;
+            curr.next = newNode;
+            curr = newNode.next;
         }
-        curr=head;
-        Node temp=res;
-        while(temp!=null){
-            temp.random=map.get(curr.random);
-            temp=temp.next;
-            curr=curr.next;
 
+        // Step 2: Set random pointers for the cloned nodes
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
         }
-        return res;
+
+        // Step 3: Separate the original and cloned lists
+        Node original = head, clone = head.next, cloneHead = head.next;
+        while (original != null) {
+            original.next = clone.next;
+            original = original.next;
+            if (clone.next != null) {
+                clone.next = clone.next.next;
+            }
+            clone = clone.next;
+        }
+
+        return cloneHead;
     }
 }
